9b4ed64fb49c4fabcd965303118474359b4e81a6
666lexa666 <konoplevlesa5@gmail.com>
2025-09-16 14:09:32 +0300

    Update 2.9

--- pages/api/operations/[opId]/qr-status.js
+++ pages/api/operations/[opId]/qr-status.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
     } else {
       return res.status(200).json({
         result: {
-          operation_status_code: null
+          operation_status_code: 1
         }
       });
     }
